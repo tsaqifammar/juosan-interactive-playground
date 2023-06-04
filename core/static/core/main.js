f251df9e2cbb31ea67ede7af7240b78ac35decbc
Muhammad Tsaqif Ammar <tsaqifammarmuh@gmail.com>
2023-06-04 23:13:42 +0700

    Add functionality for adding constraints + auto-adjusting font-size

--- core/static/core/main.js
+++ core/static/core/main.js
@@ -1,28 +1,16 @@
 /* Const */
 const MINMAX_SIZE = [1, 50];
-const GRID_WIDTH = {
-  1: "20%",
-  10: "30%",
-  20: "40%",
-  30: "50%",
-  40: "60%",
-};
-const DIRS = {
-  "Top": [-1, 0],
-  "Left": [0, -1],
-  "Bottom": [1, 0],
-  "Right": [0, 1],
-};
+const GRID_WIDTH = { 1: "25%", 10: "40%", 20: "50%", 30: "75%", 40: "90%" };
+const FONT_SIZE = { 1: "medium", 10: "small", 20: "x-small", 30: "x-small", 40: "xx-small" }
+const DIRS = { "Top": [-1, 0], "Left": [0, -1], "Bottom": [1, 0], "Right": [0, 1] };
 const NO_WALL_STYLE = "dashed 1px lightgray";
 const WALL_STYLE = "solid 1px black";
-const TOOLS = {
-  ADD_TERRITORY: "add-territory",
-  ADD_CONSTRAINT: "add-constraint",
-}
+const TOOLS = { ADD_TERRITORY: "add-territory", ADD_CONSTRAINT: "add-constraint" }
 
 /* Tracked states */
 let m = 5, n = 5, r = 1;
 let territoryNums = Array.from(Array(5), _ => Array(5).fill(0));
+let territorySizes = { 0: m*n };
 let constraintNumbers = { 0: -1 };
 let cellDivs = [];
 let messageDiv = document.getElementById("message");
@@ -39,7 +27,11 @@ function resetAddTerritoryStates() {
 }
 
 // States for "add constraint" tool
-let currentConstraintNumber = 1;
+let topCornerTerritories = { 0: [0, 0] };
+function setEditables(value) {
+  for (const [x, y] of Object.values(topCornerTerritories))
+    cellDivs[x][y].getElementsByTagName("p")[0].contentEditable = value;
+}
 
 // states undo
 let changesHistory = []; // { onUndo: () => {} } where onUndo is executed when popped
@@ -69,6 +61,12 @@ function setupPuzzleSizeSettingsWidget() {
           break;
         }
       }
+      for (const minCellCount of Object.keys(FONT_SIZE).reverse()) {
+        if (n >= Number(minCellCount)) {
+          gridDiv.style.fontSize = FONT_SIZE[minCellCount];
+          break;
+        }
+      }
       generateInitialGrid();
     }
   });
@@ -78,10 +76,13 @@ function setupTools() {
   const handleToolChange = (toolName) => {
     if (toolName === TOOLS.ADD_TERRITORY) {
       selectedTool = TOOLS.ADD_TERRITORY;
-      messageDiv.textContent = "Pick two opposite corners of the territory to create.";
+      messageDiv.textContent = "Pick two opposite corners of the territory.";
+      setEditables(false);
     } else {
       selectedTool = TOOLS.ADD_CONSTRAINT;
+      messageDiv.textContent = "Select a territory to add a constraint.";
       resetAddTerritoryStates();
+      setEditables(true);
     }
   }
   document.getElementById("add-territory").addEventListener("change", (e) => handleToolChange(e.target.value));
@@ -96,7 +97,7 @@ function setupToolWidgets() {
 /* =========================================== */
 
 function handleClick(i, j) {
-  if (selectedTool === TOOLS.ADD_TERRITORY) {
+  const handleAddTerritory = () => {
     if (cornersSelected === null) {
       cellDivs[i][j].style.backgroundColor = "green";
       cornersSelected = [i, j];
@@ -109,8 +110,19 @@ function handleClick(i, j) {
       drawTerritories();
       resetAddTerritoryStates();
     }
+  };
+
+  const handleAddConstraint = () => {
+    const t = territoryNums[i][j];
+    const [iTop, jTop] = topCornerTerritories[t];
+    if (i !== iTop || j !== jTop)
+      cellDivs[iTop][jTop].getElementsByTagName("p")[0].focus();
+  };
+
+  if (selectedTool === TOOLS.ADD_TERRITORY) {
+    handleAddTerritory();
   } else {
-    ;
+    handleAddConstraint();
   }
 }
 
@@ -120,24 +132,51 @@ function generateInitialGrid() {
 
   const gridDiv = document.getElementById("grid");
   gridDiv.replaceChildren();
+
+  const generateCell = (i, j) => {
+    const cellDiv = document.createElement("div");
+    cellDiv.classList.add("cell");
+    cellDiv.classList.add("door-cell-top");
+    cellDiv.id = `cell-${i}-${j}`;
+    const p = document.createElement("p");
+    p.addEventListener("keydown", (e) => {
+      const key = e.key;
+      if (!isNumeric(key) && !isTypableKey(key))
+        e.preventDefault();
+      
+      if (isNumeric(key)) {
+        value = parseInt(e.target.textContent + key);
+        if (value < 1 || value > territorySizes[territoryNums[i][j]]) {
+          alert("Constraint is larger than territory size.")
+          e.preventDefault();
+        } else {
+          constraintNumbers[territoryNums[i][j]] = value;
+        }
+      }
+    });
+
+    cellDiv.appendChild(p);
+    cellDiv.addEventListener("click", () => handleClick(i, j));
+    return cellDiv;
+  };
+
   for (let i = 0; i < m; i++) {
     let currentRow = [];
     for (let j = 0; j < n; j++) {
-      const cellDiv = document.createElement("div");
-      cellDiv.classList.add("cell");
-      cellDiv.classList.add("door-cell-top");
-      cellDiv.id = `cell-${i}-${j}`;
-      cellDiv.addEventListener("click", () => handleClick(i, j));
+      const cellDiv = generateCell(i, j);
       gridDiv.appendChild(cellDiv);
       currentRow.push(cellDiv);
     }
     cellDivs.push(currentRow);
   }
+  document.getElementById("add-territory").checked = false;
+  document.getElementById("add-constraint").checked = false;
   drawTerritories();
 }
 
 function recalculateTerritories() {
   let vis = {};
+  territorySizes = {}, topCornerTerritories = {};
   function dfs(i, j, num) {
     vis[[i,j]] = true;
     for (const dir in DIRS) {
@@ -147,6 +186,14 @@ function recalculateTerritories() {
         dfs(adjI, adjJ, num);
     }
     territoryNums[i][j] = num;
+    territorySizes[num] = (num in territorySizes) ? ++territorySizes[num] : 1;
+    if (num in topCornerTerritories) {
+      const [curI, curJ] = topCornerTerritories[num];
+      if (i < curI || (i == curI && j < curJ))
+        topCornerTerritories[num] = [i, j];
+    } else {
+      topCornerTerritories[num] = [i, j];
+    }
   }
 
   let currentTerritoryNum = 0;
@@ -182,3 +229,5 @@ main();
 
 function clamp(num, min, max) { return Math.min(Math.max(num, min), max); }
 function validCell(x, y) { return x >= 0 && x < m && y >= 0 && y < n; }
+function isNumeric(s) { return /^\d$/.test(s); }
+function isTypableKey(key) { return ["Backspace", "Delete", "ArrowLeft", "ArrowRight", "Home", "End"].includes(key); }
